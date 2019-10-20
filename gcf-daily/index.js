140577a5fa0bb1d88c9e275ababafe2426c37560
Cyrill Brito <cyrill.brito@gmail.com>
2019-10-20 20:16:27 +0100

    Added ynab cloud dunction and updated tsconfig target

--- gcf-daily/index.js
+++ gcf-daily/index.js
@@ -1,21 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var coinbase_pro_1 = require("coinbase-pro");
-var key = process.env.CBPRO_KEY || "";
-var secret = process.env.CBPRO_SECRET || "";
-var passphrase = process.env.CBPRO_PASSPHRASE || "";
-var apiURI = "https://api.pro.coinbase.com";
-var sandboxURI = "https://api-public.sandbox.pro.coinbase.com";
-var authedClient = new coinbase_pro_1.AuthenticatedClient(key, secret, passphrase, sandboxURI);
-exports.daily = function (req, res) {
-    var order = {
+const coinbase_pro_1 = require("coinbase-pro");
+const key = process.env.CBPRO_KEY || "";
+const secret = process.env.CBPRO_SECRET || "";
+const passphrase = process.env.CBPRO_PASSPHRASE || "";
+const apiURI = "https://api.pro.coinbase.com";
+const sandboxURI = "https://api-public.sandbox.pro.coinbase.com";
+const authedClient = new coinbase_pro_1.AuthenticatedClient(key, secret, passphrase, sandboxURI);
+exports.daily = (req, res) => {
+    const order = {
         type: "market",
         side: "buy",
         product_id: "BTC-EUR",
         size: null,
         funds: "10"
     };
-    authedClient.placeOrder(order).then(function (orderResult) {
+    authedClient.placeOrder(order).then(orderResult => {
         res.status(200).send(JSON.stringify(orderResult));
     });
 };
