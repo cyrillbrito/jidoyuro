95fb859ae50a479d7f21dc3c0d99bff7b9d8abe7
Cyrill Brito <cyrill.brito@gmail.com>
2019-11-01 12:33:33 +0000

    Fix ynab call problems

--- gcf-ynab/index.js
+++ gcf-ynab/index.js
@@ -2,12 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const puppeteer = require("puppeteer");
 const ynab = require("ynab");
-const mbcpUserCode = process.env.MBCP_USER_CODE || "";
-const mbcpAccessCode = process.env.MBCP_ACCESS_CODE || "";
-const ynabAccessToken = process.env.YNAB_ACCESS_TOKEN || "";
-const ynabAccountId = process.env.YNAB_ACCOUNT_ID || "";
+const mbcpUserCode = process.env.MBCP_USER_CODE || '';
+const mbcpAccessCode = process.env.MBCP_ACCESS_CODE || '';
+const ynabAccessToken = process.env.YNAB_ACCESS_TOKEN || '';
+const ynabBudgetId = process.env.YNAB_BUDGET_ID || '';
+const ynabAccountId = process.env.YNAB_ACCOUNT_ID || '';
 const browserPromise = puppeteer.launch({
-    args: ["--no-sandbox"]
+    args: ['--no-sandbox']
 });
 const ynabApi = new ynab.API(ynabAccessToken);
 let page;
@@ -15,87 +16,97 @@ exports.ynab = async (req, res) => {
     const browser = await browserPromise;
     const browserContext = await browser.createIncognitoBrowserContext();
     page = await browserContext.newPage();
-    sReport("open-login");
-    await page.goto("https://ind.millenniumbcp.pt/_layouts/BCP.SDC.FEP.Foundation.Presentation/Login.aspx");
+    sReport('open-login');
+    await page.goto('https://ind.millenniumbcp.pt/_layouts/BCP.SDC.FEP.Foundation.Presentation/Login.aspx');
     await eReport();
-    sReport("cookies");
-    await page.click("#btnAcceptS1CookiesV2");
+    sReport('cookies');
+    await page.click('#btnAcceptS1CookiesV2');
     await eReport();
-    sReport("fill-user-code");
-    await page.$eval("#TextBoxLogin_txField", (el, userCode) => el.value = userCode, mbcpUserCode);
+    sReport('fill-user-code');
+    await page.$eval('#TextBoxLogin_txField', (el, userCode) => el.value = userCode, mbcpUserCode);
     await eReport();
-    sReport("open-access-code");
-    await page.click("#btnPositions");
+    sReport('open-access-code');
+    await page.click('#btnPositions');
     await eReport();
-    sReport("wait-for-labels");
-    await page.waitForSelector("#lblPosition_1");
+    sReport('wait-for-labels');
+    await page.waitForSelector('#lblPosition_1');
     await eReport();
-    sReport("fill-access-code");
+    sReport('fill-access-code');
     for (let i = 1; i < 4; i++) {
-        const lbl = await page.$eval("#lblPosition_" + i, el => el.textContent);
+        const lbl = await page.$eval('#lblPosition_' + i, el => el.textContent);
         if (!lbl) {
             return;
         }
         const pos = +lbl.charAt(0) - 1;
         const code = mbcpAccessCode.charAt(pos);
-        await page.$eval("#txtPosition_" + i, (el, code) => { el.value = code; }, code);
+        await page.$eval('#txtPosition_' + i, (el, code) => { el.value = code; }, code);
     }
     await eReport();
-    sReport("click-validate");
-    await page.click("#btnValidate");
+    sReport('click-validate');
+    await page.click('#btnValidate');
     await eReport();
-    sReport("wait-more-movements");
-    await page.waitForSelector("#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_UltimosMovimentos_divOperationInfo_hlMaisMovimentos");
+    sReport('wait-more-movements');
+    await page.waitForSelector('#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_UltimosMovimentos_divOperationInfo_hlMaisMovimentos');
     await eReport();
-    sReport("click-more-movements");
-    await page.click("#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_UltimosMovimentos_divOperationInfo_hlMaisMovimentos");
+    sReport('click-more-movements');
+    await page.click('#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_UltimosMovimentos_divOperationInfo_hlMaisMovimentos');
     await eReport();
-    sReport("wait-movements");
-    await page.waitForSelector("#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_divOperationInfo_gridMovements");
+    sReport('wait-movements');
+    await page.waitForSelector('#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_divOperationInfo_gridMovements');
     await eReport();
-    sReport("read-movements");
-    const movements = await page.$eval("#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_divOperationInfo_gridMovements tbody", tbody => {
+    sReport('read-movements');
+    const movements = await page.$eval('#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_divOperationInfo_gridMovements tbody', tbody => {
         const movements = [];
         for (let i = 1; i < tbody.childNodes.length - 1; i++) {
             const cells = tbody.childNodes[i].childNodes;
             movements.push({
-                date1: (cells[1].textContent || "").trim(),
-                date2: (cells[2].textContent || "").trim(),
+                date: (cells[1].textContent || "").trim(),
                 description: (cells[3].textContent || "").trim(),
-                amount: +(cells[4].textContent || "").trim().replace(".", "").replace(",", ".")
+                amount: (cells[4].textContent || "").trim(),
+                balance: (cells[5].textContent || "").trim()
             });
         }
         return movements;
     });
     await eReport();
-    sReport("map-movements");
+    sReport('map-movements');
     const transactions = [];
     for (const movement of movements) {
-        const importNumber = transactions.filter(t => t.date === movement.date1 && t.payee_name === movement.description && t.amount === movement.amount).length + 1;
+        movement.date = movement.date.substring(6, 10) + movement.date.substring(2, 6) + movement.date.substring(0, 2);
+        const iidDate = movement.date.replace(/-/g, '');
+        const iidDescription = movement.description.replace(/[^A-Za-z]|COMPRA|CONTACTLESS/g, '');
+        const iidAmount = movement.amount.replace(/\.|,/g, '');
+        const iidBalance = movement.balance.replace(/\.|,/g, '');
         transactions.push({
             account_id: ynabAccountId,
-            date: movement.date1,
-            amount: movement.amount,
+            date: movement.date,
+            amount: +movement.amount.replace('.', '').replace(',', '.'),
             payee_name: movement.description,
             cleared: ynab.SaveTransaction.ClearedEnum.Cleared,
-            import_id: `${movement.date1}:${movement.description}:${movement.amount}:${importNumber}`
+            import_id: `${iidDate}${iidAmount}${iidBalance}${iidDescription}`.substring(0, 36)
         });
     }
     await eReport();
-    sReport("send-ynab");
-    const response = await ynabApi.transactions.createTransactions("default", { transactions: transactions });
+    sReport('send-ynab');
+    let response;
+    try {
+        response = await ynabApi.transactions.createTransactions(ynabBudgetId, { transactions: transactions });
+    }
+    catch (e) {
+        console.log(e);
+    }
     await eReport();
-    res.send({ transactions, response });
+    res.send({ movements, transactions, response });
     await browserContext.close();
 };
 let report;
 let reportNumber = 0;
 function sReport(s) {
     report = s;
-    console.log("[S]", s);
+    console.log('[S]', s);
 }
 async function eReport() {
-    console.log("[E]", report);
-    // await page.screenshot({ path: reportNumber + "-" + report + ".png" });
+    console.log('[E]', report);
+    // await page.screenshot({ path: reportNumber + '-' + report + '.png' });
     reportNumber++;
 }
--- gcf-ynab/index.ts
+++ gcf-ynab/index.ts
@@ -1,21 +1,22 @@
-import * as express from "express";
-import * as puppeteer from "puppeteer";
-import * as ynab from "ynab";
+import * as express from 'express';
+import * as puppeteer from 'puppeteer';
+import * as ynab from 'ynab';
 
 interface Movement {
-  date1: string;
-  date2: string;
+  date: string;
   description: string;
-  amount: number;
+  amount: string;
+  balance: string;
 }
 
-const mbcpUserCode = process.env.MBCP_USER_CODE || "";
-const mbcpAccessCode = process.env.MBCP_ACCESS_CODE || "";
-const ynabAccessToken = process.env.YNAB_ACCESS_TOKEN || "";
-const ynabAccountId = process.env.YNAB_ACCOUNT_ID || "";
+const mbcpUserCode = process.env.MBCP_USER_CODE || '';
+const mbcpAccessCode = process.env.MBCP_ACCESS_CODE || '';
+const ynabAccessToken = process.env.YNAB_ACCESS_TOKEN || '';
+const ynabBudgetId = process.env.YNAB_BUDGET_ID || '';
+const ynabAccountId = process.env.YNAB_ACCOUNT_ID || '';
 
 const browserPromise = puppeteer.launch({
-  args: ["--no-sandbox"]
+  args: ['--no-sandbox']
 });
 
 const ynabApi = new ynab.API(ynabAccessToken);
@@ -28,90 +29,103 @@ exports.ynab = async (req: express.Request, res: express.Response) => {
   const browserContext = await browser.createIncognitoBrowserContext();
   page = await browserContext.newPage();
 
-  sReport("open-login");
-  await page.goto("https://ind.millenniumbcp.pt/_layouts/BCP.SDC.FEP.Foundation.Presentation/Login.aspx");
+  sReport('open-login');
+  await page.goto('https://ind.millenniumbcp.pt/_layouts/BCP.SDC.FEP.Foundation.Presentation/Login.aspx');
   await eReport();
 
-  sReport("cookies");
-  await page.click("#btnAcceptS1CookiesV2");
+  sReport('cookies');
+  await page.click('#btnAcceptS1CookiesV2');
   await eReport();
 
-  sReport("fill-user-code");
-  await page.$eval("#TextBoxLogin_txField", (el: any, userCode) => el.value = userCode, mbcpUserCode);
+  sReport('fill-user-code');
+  await page.$eval('#TextBoxLogin_txField', (el: any, userCode) => el.value = userCode, mbcpUserCode);
   await eReport();
 
-  sReport("open-access-code");
-  await page.click("#btnPositions");
+  sReport('open-access-code');
+  await page.click('#btnPositions');
   await eReport();
 
-  sReport("wait-for-labels");
-  await page.waitForSelector("#lblPosition_1");
+  sReport('wait-for-labels');
+  await page.waitForSelector('#lblPosition_1');
   await eReport();
 
-  sReport("fill-access-code");
+  sReport('fill-access-code');
   for (let i = 1; i < 4; i++) {
-    const lbl = await page.$eval("#lblPosition_" + i, el => el.textContent);
+    const lbl = await page.$eval('#lblPosition_' + i, el => el.textContent);
     if (!lbl) {
       return;
     }
     const pos = +lbl.charAt(0) - 1;
     const code = mbcpAccessCode.charAt(pos);
-    await page.$eval("#txtPosition_" + i, (el: any, code) => { el.value = code }, code);
+    await page.$eval('#txtPosition_' + i, (el: any, code) => { el.value = code }, code);
   }
   await eReport();
 
-  sReport("click-validate");
-  await page.click("#btnValidate");
+  sReport('click-validate');
+  await page.click('#btnValidate');
   await eReport();
 
-  sReport("wait-more-movements");
-  await page.waitForSelector("#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_UltimosMovimentos_divOperationInfo_hlMaisMovimentos");
+  sReport('wait-more-movements');
+  await page.waitForSelector('#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_UltimosMovimentos_divOperationInfo_hlMaisMovimentos');
   await eReport();
 
-  sReport("click-more-movements");
-  await page.click("#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_UltimosMovimentos_divOperationInfo_hlMaisMovimentos");
+  sReport('click-more-movements');
+  await page.click('#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_UltimosMovimentos_divOperationInfo_hlMaisMovimentos');
   await eReport();
 
-  sReport("wait-movements");
-  await page.waitForSelector("#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_divOperationInfo_gridMovements");
+  sReport('wait-movements');
+  await page.waitForSelector('#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_divOperationInfo_gridMovements');
   await eReport();
 
-  sReport("read-movements");
-  const movements = await page.$eval("#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_divOperationInfo_gridMovements tbody", tbody => {
+  sReport('read-movements');
+  const movements = await page.$eval('#ctl00_ctl00_PlaceHolderMainBase_PlaceHolderMain__bcpTransactionContainer_ctl01_divOperationInfo_gridMovements tbody', tbody => {
     const movements: Movement[] = [];
     for (let i = 1; i < tbody.childNodes.length - 1; i++) {
       const cells = tbody.childNodes[i].childNodes;
       movements.push({
-        date1: (cells[1].textContent || "").trim(),
-        date2: (cells[2].textContent || "").trim(),
+        date: (cells[1].textContent || "").trim(),
         description: (cells[3].textContent || "").trim(),
-        amount: +(cells[4].textContent || "").trim().replace(".", "").replace(",", ".")
+        amount: (cells[4].textContent || "").trim(),
+        balance: (cells[5].textContent || "").trim()
       });
     }
     return movements;
   });
   await eReport();
 
-  sReport("map-movements");
+
+  sReport('map-movements');
   const transactions: ynab.SaveTransaction[] = [];
   for (const movement of movements) {
-    const importNumber = transactions.filter(t => t.date === movement.date1 && t.payee_name === movement.description && t.amount === movement.amount).length + 1;
+
+    movement.date = movement.date.substring(6, 10) + movement.date.substring(2, 6) + movement.date.substring(0, 2);
+
+    const iidDate = movement.date.replace(/-/g, '');
+    const iidDescription = movement.description.replace(/[^A-Za-z]|COMPRA|CONTACTLESS/g, '');
+    const iidAmount = movement.amount.replace(/\.|,/g, '');
+    const iidBalance = movement.balance.replace(/\.|,/g, '');
+
     transactions.push({
       account_id: ynabAccountId,
-      date: movement.date1,
-      amount: movement.amount,
+      date: movement.date, // invert date
+      amount: +movement.amount.replace('.', '').replace(',', '.'),
       payee_name: movement.description,
       cleared: ynab.SaveTransaction.ClearedEnum.Cleared,
-      import_id: `${movement.date1}:${movement.description}:${movement.amount}:${importNumber}`
+      import_id: `${iidDate}${iidAmount}${iidBalance}${iidDescription}`.substring(0, 36)
     });
   }
   await eReport();
 
-  sReport("send-ynab");
-  const response = await ynabApi.transactions.createTransactions("default", { transactions: transactions });
+  sReport('send-ynab');
+  let response;
+  try {
+    response = await ynabApi.transactions.createTransactions(ynabBudgetId, { transactions: transactions });
+  } catch (e) {
+    console.log(e);
+  }
   await eReport();
 
-  res.send({ transactions, response });
+  res.send({ movements, transactions, response });
 
   await browserContext.close();
 };
@@ -121,11 +135,11 @@ let reportNumber = 0;
 
 function sReport(s: string): void {
   report = s;
-  console.log("[S]", s);
+  console.log('[S]', s);
 }
 
 async function eReport(): Promise<void> {
-  console.log("[E]", report);
-  // await page.screenshot({ path: reportNumber + "-" + report + ".png" });
+  console.log('[E]', report);
+  // await page.screenshot({ path: reportNumber + '-' + report + '.png' });
   reportNumber++;
 }
