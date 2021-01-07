b3156d01b231658f42bd42119311da1cdbda2531
Cyrill Brito <cyrill.brito@gmail.com>
2021-01-07 16:57:26 +0000

    Test secret service

--- run/src/app.ts
+++ run/src/app.ts
@@ -1,12 +1,21 @@
 import express, { Request, Response } from 'express';
-import { ActivoImporter } from './activo-importer';
+// import { ActivoImporter } from './activo-importer';
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 
+
+const smsc = new SecretManagerServiceClient();
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.get('/', async (request: Request, response: Response) => {
-  await new ActivoImporter({ multichannelCode: '', userCode: '', ynab: { accessToken: '', accountId: '' } }).import();
-  response.send('OK');
+  // await new ActivoImporter({ multichannelCode: '', userCode: '', ynab: { accessToken: '', accountId: '' } }).import();
+  const [secret] = await smsc.getSecret({
+    name: 'sample-test',
+  });
+
+  console.log(secret);
+
+  response.send('OK ' + secret);
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
